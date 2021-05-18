2c4f410ff30aeb0dc34254acd05c8380f4d59882
nicolas-bitbase <nicolas@bitbase.es>
2021-05-18 11:04:43 +0000

    arreglado

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
- let pronoun = ['the','our'];
-  let adj = ['great', 'big' ];
-  let noun = ['jogger','racoon'];
- let extension = [".com", ".net", ".us", ".io"]
 
-  for(let i = 0; i < pronoun.length; i++ ){
-          for(let j = 0; j < adj.length; j++ ){
-              for(let z = 0; z < noun.length; z++ ){
-                  for(let y = 0; y < extension.length; y++ ){
-                      console.log(pronoun[i] + adj[j] + noun[z] + extension[y]);
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let com = ['.com', '.net', '.us', '.io'];
 
-                  }
+function f_genDomain(array1, array2, array3, array4) {
+    arrays = [];
+    array_position = 0;
 
-                   
-              }
-       }
- 
- }
+    for (i = 0; i < array1.length; i++) {
+        for (j = 0; j < array2.length; j++) {
+            for (k = 0; k < array3.length; k++) {
+                for (p = 0; p < array4.length; p++) {
+                    arrays.push(array1[i] + array2[j] + array3[k] + array4[p]);
+                    document.write(arrays[array_position] + "<br>");
+                    array_position++;
+                }
+            }
+        }
+    }
+    return arrays;
+}
+
+
+f_genDomain(pronoun, adj, noun, com);
